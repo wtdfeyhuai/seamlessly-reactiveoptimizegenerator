acc16277a2fb1cc3fa6cd07c98f1d323cf52009c
wtdfeyhuai <gs1taker181@hotmail.com>
2024-05-10 07:35:04 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-function groupAnagrams(strs) {
-  const map = new Map();
-  for (const str of strs) {
-    const sortedStr = str.split("").sort().join("");
-    if (!map.has(sortedStr)) map.set(sortedStr, []);
-    map.get(sortedStr).push(str);
+const linearSearch = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
   }
-  return [...map.values()];
-}
+  return -1;
+};
